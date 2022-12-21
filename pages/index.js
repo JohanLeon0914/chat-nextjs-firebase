44c9222f0947fdf7a130e87ff8c3512b96222f7f
JohanLeon0914 <johanalbertols@ufps.edu.co>
2022-12-21 13:15:41 -0500

    arreglando sidebar en index

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { Box } from '@chakra-ui/react'
 import Head from 'next/head'
-import Sidebar from '../components/SideBar'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
